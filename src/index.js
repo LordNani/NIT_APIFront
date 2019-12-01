311f451118ab2e335fb747f78bfb825885959f42
LordNani <47566320+LordNani@users.noreply.github.com>
2019-12-01 23:12:37 +0200

    Trying to evolve to es6

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './scss/main.scss'
 import './scss/bootstrap/bootstrap.min.css';
 import 'bootstrap';
 
-var cart = (function() {
+const cart = (function() {
     const cartStorage = [];
     let nodes = [];
 
@@ -14,16 +14,16 @@ var cart = (function() {
       this.count = count;
   }
 
-  var object = {};
+  const object = {};
 
   object.addProduct = function(node, name, price, count) {
-      for (var i in cartStorage) {
-          if (cartStorage[i].name === name) {
-              cartStorage[i].count++;
+      for (var item of cartStorage) {
+          if (item.name === name) {
+            item.count++;
               return;
           }
       }
-      var newItem = new Product(name, price, count);
+      const newItem = new Product(name, price, count);
       // console.log(newItem.name + ' ' + newItem.price);
       cartStorage.push(newItem);
       document.getElementById('cont1').appendChild(node);
@@ -31,11 +31,11 @@ var cart = (function() {
   }
 
   object.removeOneProduct = function(node, name) {
-      for (var i in cartStorage) {
-          if (cartStorage[i].name === name) {
-              cartStorage[i].count--;
-              if (cartStorage[i].count <= 0) {
-                  cartStorage.splice(i, 1);
+      for (var item in cartStorage) {
+          if (cartStorage[item].name === name) {
+              cartStorage[item].count--;
+              if (cartStorage[item].count <= 0) {
+                  cartStorage.splice(item, 1);
                   for (var nd in nodes) {
                       if (nodes[nd].childNodes[3].getAttribute('data-name') == node.childNodes[3].getAttribute('data-name')) {
                           document.getElementById('cont1').removeChild(nodes[nd]);
@@ -50,19 +50,26 @@ var cart = (function() {
 
   object.clearCart = function() {
       cartStorage.splice(0, cartStorage.length);
-      for (var nd in nodes)
-          document.getElementById('cont1').removeChild(nodes[nd]);
+      for (var nd of nodes)
+          document.getElementById('cont1').removeChild(nd);
       nodes = [];
   }
 
   object.totalSum = function() {
-      var totalSum = 0;
-      for (var i in cartStorage) {
-          totalSum += cartStorage[i].price * cartStorage[i].count;
+      let totalSum = 0;
+      for (var item of cartStorage) {
+          totalSum += item.price * item.count;
       }
       return Number(totalSum.toFixed(2));
   }
 
+  object.changeCount = function(node, name, quantity) {
+    for (var item of cartStorage) {
+        if (item.name == name) {
+            node.childNodes[5].innerHTML = (name + '&nbsp&nbsp Total: ' + (item.count + quantity));
+        }
+    }
+  };
   return object;
 })();
 
@@ -74,43 +81,38 @@ $('.clearCart').click(function() {
 
 $('.btn-buy').click(function(event) {
   event.preventDefault();
-  var name = $(this).parent().data('name');
-  var price = Number($(this).parent().data('price'));
+  const name = $(this).parent().data('name');
+  const price = Number($(this).parent().data('price'));
   // console.log(name);
-  var nodeCopy = event.target.parentNode.parentNode.cloneNode(true);
+  const nodeCopy = event.target.parentNode.parentNode.cloneNode(true);
   // console.log(nodeCopy.childNodes[3].childNodes[1].nodeName);
-  var minus = document.createElement('button');
-  var plus = document.createElement('button');
+  const minus = document.createElement('button');
+  const plus = document.createElement('button');
   minus.className = "btn btn-danger btn-md minus-count";
   plus.className = "btn btn-success btn-md plus-count";
   plus.innerHTML = "+";
   minus.innerHTML = "-";
   nodeCopy.childNodes[3].childNodes[1].replaceWith(minus);
   nodeCopy.childNodes[3].appendChild(plus);
-  $("<button/>", {
-    "class": "btn btn-success btn-md plus-count",
-    text: "+"
-    })
-    .appendTo(nodeCopy.childNodes[3]);
-  for (var node in nodes) {
-      if (nodes[node].childNodes[3].getAttribute('data-name') == name) {
-          changeCount(nodes[node], name, 1);
+  for (var node of nodes) {
+      if (node.childNodes[3].getAttribute('data-name') == name) {
+          cart.changeCount(node, name, 1);
       }
   }
   cart.addProduct(nodeCopy, name, price, 1);
   $('.totalSum').html('Total sum: ' + cart.totalSum() + '$');
 
 
-  var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-  var newCart;
+  const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+  let newCart = 0;
   if (width < 1270)
       newcart = $('.dropdownIcon');
   else
       newcart = $('.open-cart');
 
-  var imgtodrag = $(this).parent().parent('.card').find("img").eq(0);
+  const imgtodrag = $(this).parent().parent('.card').find("img").eq(0);
   if (imgtodrag) {
-      var imgclone = imgtodrag.clone()
+      const imgclone = imgtodrag.clone()
           .offset({
               top: imgtodrag.offset().top,
               left: imgtodrag.offset().left
@@ -142,27 +144,20 @@ $('.btn-buy').click(function(event) {
 
 $(document).on("click", ".minus-count", function() {
   //console.log('minus content');
-  var name = $(this).parent().data('name');
+  const name = $(this).parent().data('name');
   // console.log(name);
-  var nodeCopy = event.target.parentNode.parentNode.cloneNode(true);
+  const nodeCopy = event.target.parentNode.parentNode.cloneNode(true);
   // console.log(nodeCopy.childNodes[3].nodeName);
-  changeCount(event.target.parentNode.parentNode, name, -1);
+  cart.changeCount(event.target.parentNode.parentNode, name, -1);
   cart.removeOneProduct(nodeCopy, name);
   $('.totalSum').html('Total sum: ' + cart.totalSum() + '$');
 });
 
 $(document).on("click", ".plus-count", function() {
-  var name = $(this).parent().data('name');
-  var price = Number($(this).parent().data('price'));
-  changeCount(event.target.parentNode.parentNode, name, 1);
+    const name = $(this).parent().data('name');
+    const price = Number($(this).parent().data('price'));
+  cart.changeCount(event.target.parentNode.parentNode, name, 1);
   cart.addProduct(null, name, price, 1);
   $('.totalSum').html('Total sum: ' + cart.totalSum() + '$');
 });
 
-function changeCount(node, name, quantity) {
-  for (var i in cartStorage) {
-      if (cartStorage[i].name == name) {
-          node.childNodes[5].innerHTML = (name + '&nbsp&nbsp Total: ' + (cartStorage[i].count + quantity));
-      }
-  }
-}
