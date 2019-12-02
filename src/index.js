259b788f31027710cda8b8470544a9faa94191d1
LordNani <47566320+LordNani@users.noreply.github.com>
2019-12-02 14:34:18 +0200

    Trying to generate get's

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-element-newline */
 console.log("Hello webpack!");
 console.log(`The time is ${new Date()}`);
 import './scss/bootstrap/bootstrap.min.css';
@@ -5,6 +6,8 @@ import 'bootstrap';
 import './scss/main.scss';
 const cartStorage = [];
 const nodes = [];
+
+const itemsMap = new Map();
 
 function Product(name, price, count) {
     this.name = name;
@@ -74,6 +77,12 @@ $('.clearCart').click(function () {
 });
 
 $('.btn-buy').click(function (event) {
+
+    for (var [key, value] of itemsMap) {
+        console.log(key.name + ' = ' + value.length);
+    }
+
+
     event.preventDefault();
     const name = $(this).parent().data('name');
     const price = Number($(this).parent().data('price'));
@@ -96,42 +105,42 @@ $('.btn-buy').click(function (event) {
     addProduct(nodeCopy, name, price, 1);
     $('.totalSum').html('Total sum: ' + totalSum() + '$');
 
-      const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-      let newcart = $('.open-cart');
-      if (width < 1270)
-          newcart = $('.dropdownIcon');
+    const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    let newcart = $('.open-cart');
+    if (width < 1270)
+        newcart = $('.dropdownIcon');
 
 
-      const imgtodrag = $(this).parent().parent('.card').find("img").eq(0);
-      if (imgtodrag) {
-          const imgclone = imgtodrag.clone()
-              .offset({
-                  top: imgtodrag.offset().top,
-                  left: imgtodrag.offset().left
-              })
-              .css({
-                  'opacity': '0.5',
-                  'position': 'absolute',
-                  'height': '200px',
-                  'width': '200px',
-                  'z-index': '100',
-                  'border-radius': '15%'
-              })
-              .appendTo($('body'))
-              .animate({
-                  'top': newcart.offset().top + 15,
-                  'left': newcart.offset().left + 40,
-                  'width': 0,
-                  'height': 0
-              }, 1000, 'easeInOutExpo');
+    const imgtodrag = $(this).parent().parent('.card').find("img").eq(0);
+    if (imgtodrag) {
+        const imgclone = imgtodrag.clone()
+            .offset({
+                top: imgtodrag.offset().top,
+                left: imgtodrag.offset().left
+            })
+            .css({
+                'opacity': '0.5',
+                'position': 'absolute',
+                'height': '200px',
+                'width': '200px',
+                'z-index': '100',
+                'border-radius': '15%'
+            })
+            .appendTo($('body'))
+            .animate({
+                'top': newcart.offset().top + 15,
+                'left': newcart.offset().left + 40,
+                'width': 0,
+                'height': 0
+            }, 1000, 'easeInOutExpo');
 
-          imgclone.animate({
-              'width': 0,
-              'height': 0
-          }, function() {
-              $(this).detach()
-          });
-      }
+        imgclone.animate({
+            'width': 0,
+            'height': 0
+        }, function () {
+            $(this).detach()
+        });
+    }
 
 });
 
@@ -158,3 +167,35 @@ $(document).on("click", ".plus-count", function () {
 $("#dropdownImage").on("click", function () {
     $("#myDropdown").toggle("show");
 });
+
+window.onload = function () {
+    $.get({
+        dataType: "json",
+        url: "https://nit.tron.net.ua/api/category/list",
+        success(data) {
+            console.log(data);
+            for (var category of data) {
+                itemsMap.set(category.id, []);
+            }
+            itemsMap.set()
+            /* data.forEach(function(item) {
+               console.log(item.name);
+               }); */
+        }
+
+        /*  let tempElem = '<li class="category"><button id="category_all">All</button></li>';
+           $(".categories_list").append(temp_elem);
+           $("#category_all").on("click", function (event) {
+             show_category("all");
+           });
+           for (let i = 0; i < data.length; i++) {
+             temp_elem = '<li class="category"><button id="category_' + data[i].id + '">' + data[i].name + '</button></li>';
+             $(".categories_list").append(temp_elem);
+             $('#category_' + data[i].id).on("click", function (event) {
+               show_category(data[i].id);
+             });
+             categories_json.set(data[i].id, data[i]);
+             }
+            } */
+    });
+}
